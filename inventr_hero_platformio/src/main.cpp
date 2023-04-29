4b0919564341c2c6dbfa04d00146400aa37d7bad
dvillalva21 <dvillalva21@gmail.com>
2023-04-29 03:20:03 -0600

    switch control

---
+++ inventr_hero_platformio/src/main.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+
+//outputs
+int RED = 12, 
+    GREEN = 11, 
+    YELLOW = 10;
+
+//inputs
+int Switch1 = 2, 
+    Switch2 = 3, 
+    Switch3 = 4; 
+void blink(int LED){
+    digitalWrite(LED, HIGH);
+    delay(800);
+    digitalWrite(LED, LOW);
+
+}
+void setup() {
+  //blink all lights once
+  blink(RED);
+  blink(GREEN);
+  blink(YELLOW);
+
+  
+
+}
+
+void loop() {
+  
+    if (digitalRead(Switch1) == HIGH)
+    {
+      digitalWrite(RED, HIGH);
+      digitalWrite(GREEN, HIGH);
+      digitalWrite(YELLOW, HIGH);
+    }
+    else{
+      digitalWrite(RED, LOW);
+      digitalWrite(GREEN, LOW);
+      digitalWrite(YELLOW, LOW);
+    }
+  
+
+
+}
