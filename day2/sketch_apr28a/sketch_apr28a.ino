09ae65c065d6600980f371f4271fe41741daa591
dvillalva21 <dvillalva21@gmail.com>
2023-04-28 19:30:43 -0600

    MorseCode

--- day2/sketch_apr28a/sketch_apr28a.ino
+++ day2/sketch_apr28a/sketch_apr28a.ino
@@ -1,5 +1,16 @@
 int Light = 12;
-
+void dot() {
+  digitalWrite(Light, HIGH);
+  delay(500);
+  digitalWrite(Light, LOW);
+  delay(100);
+}
+void dash() {
+  digitalWrite(Light, HIGH);
+  delay(1200);
+  digitalWrite(Light, LOW);
+  delay(100);
+}
 void setup() {
   // put your setup code here, to run once:
   pinMode(Light, OUTPUT);
@@ -8,8 +19,52 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(Light, LOW);
-delay(1000);
-digitalWrite(Light, HIGH);
-delay(1000);
+//digitalWrite(Light, LOW);
+//delay(1000);
+//digitalWrite(Light, HIGH);
+//delay(1000);
+
+//Hello World in morse code
+  dot();
+  dot();
+  dot();
+  dot();
+  delay(800);
+  dot();
+  dash();
+  dot();
+  dot();
+  delay(800);
+  dot();
+  dash();
+  dot();
+  dot();
+  delay(800);
+  dash();
+  dash();
+  dash();
+
+  delay(1500);
+  
+  dot();
+  dash();
+  dash();
+  delay(800);
+  dash();
+  dash();
+  dash();
+  delay(800);
+  dot();
+  dash();
+  dot();
+  delay(800);
+  dot();
+  dash();
+  dot();
+  dot();
+  delay(800);
+  dash();
+  dot();
+  dot();
+  delay(1500);
 }
